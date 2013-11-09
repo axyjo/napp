e7226c940ec89492ec34533dbf7d9cc41f63eb22
Akshay Joshi <me@akshayjoshi.com>
2013-11-09 17:06:43 -0500

    Add sass-bootstrap.

--- app/scripts/app/config.js
+++ app/scripts/app/config.js
@@ -9,6 +9,18 @@ require.config({
     JST: 'templates',
     underscore: '../../bower_components/lodash/dist/lodash.compat',
     backbone: '../../bower_components/backbone/backbone',
+    'bootstrap-affix': '../../bower_components/sass-bootstrap/js/affix',
+    'bootstrap-alert': '../../bower_components/sass-bootstrap/js/alert',
+    'bootstrap-button': '../../bower_components/sass-bootstrap/js/button',
+    'bootstrap-carousel': '../../bower_components/sass-bootstrap/js/carousel',
+    'bootstrap-collapse': '../../bower_components/sass-bootstrap/js/collapse',
+    'bootstrap-dropdown': '../../bower_components/sass-bootstrap/js/dropdown',
+    'bootstrap-modal': '../../bower_components/sass-bootstrap/js/modal',
+    'bootstrap-popover': '../../bower_components/sass-bootstrap/js/popover',
+    'bootstrap-scrollspy': '../../bower_components/sass-bootstrap/js/scrollspy',
+    'bootstrap-tab': '../../bower_components/sass-bootstrap/js/tab',
+    'bootstrap-tooltip': '../../bower_components/sass-bootstrap/js/tooltip',
+    'bootstrap-transition': '../../bower_components/sass-bootstrap/js/transition'
   },
   shim: {
     handlebars: {
