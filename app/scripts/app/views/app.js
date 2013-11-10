76fd598d01d8b8b565a2a9616665217208a3856c
Akshay Joshi <me@akshayjoshi.com>
2013-11-10 01:32:43 -0500

    Refactor IoC pattern for popups.

--- app/scripts/app/views/app.js
+++ app/scripts/app/views/app.js
@@ -4,21 +4,19 @@ define([
   'views/searchbox',
   'views/navigation',
   'views/popup',
+  'models/currentLocation',
   'jquery',
   'leaflet',
   'underscore'
 ],
 
-function (Backbone, template, SearchBoxView, NavBoxView, PopupView, $, L, _) {
+function (Backbone, template, SearchBoxView, NavBoxView, PopupView, CurrentLocation, $, L, _) {
   'use strict';
 
   var AppView = Backbone.View.extend({
     el: '.content',
     template: template,
-
-    events: {
-      'click': 'handleClick',
-    },
+    model: new CurrentLocation(),
 
     initialize: function initialize (options) {
       this.app = options.app;
@@ -30,15 +28,13 @@ function (Backbone, template, SearchBoxView, NavBoxView, PopupView, $, L, _) {
       //this.listenTo(this.app.collections.people, 'reset', this.populateView);
     },
 
-    distancePopup: function(point) {
-      var marker = L.marker(point).addTo(this.map);
-      var distance = Math.round(point.distanceTo(this.latlng)/10)/100;
-      return distance;
-    },
+
 
     render: function render () {
-      this.point = new L.LatLng(43.467, -80.5400)
-      this.map = L.map('map').setView(this.point, 18);
+      this.model.once('change:latlng', function() {
+        this.map.setView(this.model.get('latlng'), 18);
+      }, this);
+      this.map = L.map('map');
       L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
@@ -46,20 +42,22 @@ function (Backbone, template, SearchBoxView, NavBoxView, PopupView, $, L, _) {
       this.map.on('locationfound', _.bind(this.onLocationFound, this));
 
       this.map.locate({setView: true, maxZoom: 15});
+      setInterval(_.bind(function() {
+        console.log('firing');
+        this.map.locate({setView: true, maxZoom: 15});
+      }, this), 3000);
 
       var sbView = new SearchBoxView();
-      sbView.initialize();
       sbView.render(this.map);
 
       var navView = new NavBoxView();
-      navView.initialize();
       navView.render(this.map);
 
-      var popupView = new PopupView();
-      popupView.initialize({
-          latlng: this.point,
-          distance: this.distancePopup(this.point)      
-      })
+      new PopupView({
+        point: new L.LatLng(43.467, -80.5400),
+        currentLocation: this.model,
+        map: this.map
+      });
 
       return this;
     },
@@ -72,19 +70,19 @@ function (Backbone, template, SearchBoxView, NavBoxView, PopupView, $, L, _) {
 
     onLocationFound: function onLocationFound(e) {
       if (this.map) {
-        this.latlng = e.latlng;
         var radius = Math.round(e.accuracy * 100 / 2)/100;
-        var latitude = e.latlng;
-        this.map.marker(e.latlng).addTo(this.map)
-          .bindPopup("You are here!").openPopup();
-        L.circle(e.latlng, radius).addTo(map);
-        this.distancePopup(this.point);
-      }
-    },
+        this.model.set('latlng', e.latlng);
 
-    handleClick: function handleClick (evt) {
-      'console' in window && console.log(evt);
-    },
+        if (this.marker && this.markerCircle) {
+          this.marker.setLatLng(e.latlng);
+          this.markerCircle.setLatLng(e.latlng);
+          this.markerCircle.setRadius(radius);
+        } else {
+          this.marker = L.marker(e.latlng).addTo(this.map).bindPopup('You are here!').openPopup();
+          this.markerCircle = L.circle(e.latlng, radius).addTo(this.map);
+        }
+      }
+    }
   });
 
   return AppView;
--- app/scripts/app/views/popup.js
+++ app/scripts/app/views/popup.js
@@ -9,33 +9,37 @@ function (Backbone, $, template, L) {
   'use strict';
 
   var PopupView = Backbone.View.extend({
-    events: {
-      'click': 'handleClick',
-    },
-
     initialize: function initialize (options) {
       options = options || {};
-      this.distance = options.distance;
-      this.latlng = options.latlng;
+      this.point = options.point;
+      this.currentLocation = options.currentLocation;
+      this.map = options.map;
       this.addListeners();
     },
 
     addListeners: function addListeners () {
+      this.currentLocation.on('change:latlng', this.render, this);
     },
 
-    render: function render (map) {
-      L.marker(this.latlng).addTo(map)
-          .bindPopup(template(this)).openPopup();
+    render: function render(options) {
+      options = options || {};
+      if (this.marker) {
+        this.map.removeLayer(this.marker);
+      }
 
-      L.circle(this.latlng, this.distane).addTo(map);
+      this.marker = L.marker(this.point).addTo(this.map)
+          .bindPopup(template({distance: this._calculateDistance()}));
+
       return this;
     },
 
-    populateView: function populateView (collection) {
-    },
-
-    handleClick: function handleClick (evt) {
-      'console' in window && console.log(evt);
+    /**
+     * Returns distance from point to current location in km, two decimal places.
+     * @param  {L.Point} point
+     * @return {Number}
+     */
+    _calculateDistance: function() {
+      return Math.round(this.currentLocation.get('latlng').distanceTo(this.point)/10)/100;
     },
   });
 
